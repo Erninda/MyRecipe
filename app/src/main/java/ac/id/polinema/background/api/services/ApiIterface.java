63947adaccb1a2ca0fd96b9283c1d62d005118d7
Erninda <erninda.ristiani94@gmail.com>
2020-04-27 08:53:33 +0700

    Initial commit

---
+++ app/src/main/java/ac/id/polinema/background/api/services/ApiIterface.java
@@ -0,0 +1,19 @@
+package ac.id.polinema.background.api.services;
+
+import java.util.List;
+
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface ApiIterface {
+//    @GET("/")
+//    Call<AppVersion> getAppVersion();
+//
+//    @POST("/v1/links")
+//    Call<ResponseData> doRebrand(@Query("apikey") String apikey , @Body RebrandlyRequest destination);
+//
+//    @GET("/v1/links")
+//    Call<List<ResponseData>> getRebrand(@Query("apikey") String data);
+}
